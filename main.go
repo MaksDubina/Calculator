d2835423f11e21ef50c3f5c8288b1523c8762694
glower <mgd8832@gmail.com>
2023-12-08 11:42:50 +0700

    Init commit

---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func transformationIntoArabian(str string) int {
+	numbers := map[string]int{
+		"I":    1,
+		"II":   2,
+		"III":  3,
+		"IV":   4,
+		"V":    5,
+		"VI":   6,
+		"VII":  7,
+		"VIII": 8,
+		"IX":   9,
+		"X":    10,
+	}
+	return numbers[str]
+}
+
+func transformationIntoRoman(integer int) string {
+	result := ""
+	for integer > 0 {
+		if (integer - 10) >= 0 {
+			result += "X"
+			integer -= 10
+			continue
+		}
+		if (integer - 9) >= 0 {
+			result += "IX"
+			integer -= 9
+			continue
+		}
+		if (integer - 5) >= 0 {
+			result += "V"
+			integer -= 5
+			continue
+		}
+		if (integer - 4) >= 0 {
+			result += "IV"
+			integer -= 4
+			continue
+		}
+		if (integer - 1) >= 0 {
+			result += "I"
+			integer -= 1
+			continue
+		}
+	}
+	return result
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Введите значение")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+		operands := strings.Split(text, " ")
+		if len(operands) < 3 {
+			fmt.Println("Вывод ошибки, так как строка не является математической операцией.")
+			break
+		} else if len(operands) > 3 {
+			fmt.Println("Вывод ошибки, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *).")
+			break
+		}
+
+		symbol := operands[1]
+
+		arg1, _ := strconv.Atoi(operands[0])
+		arg2, _ := strconv.Atoi(operands[2])
+		RimArg1 := transformationIntoArabian(operands[0])
+		RimArg2 := transformationIntoArabian(operands[2])
+		fmt.Println(arg1, arg2, RimArg1, RimArg2)
+
+		if arg1 == 0 && arg2 == 0 && RimArg1 != 0 && RimArg2 != 0 {
+			switch symbol {
+			case "+":
+				fmt.Println(transformationIntoRoman(RimArg1 + RimArg2))
+			case "-":
+				if (RimArg1 - RimArg2) > 0 {
+					fmt.Println(transformationIntoRoman(RimArg1 - RimArg2))
+				} else {
+					fmt.Println("Вывод ошибки, так как в римской системе нет отрицательных чисел.")
+				}
+
+			case "/":
+				fmt.Println(transformationIntoRoman(RimArg1 / RimArg2))
+			case "*":
+				fmt.Println(transformationIntoRoman(RimArg1 * RimArg2))
+			default:
+				fmt.Println("Вывод ошибки, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *).")
+			}
+		} else if arg1 != 0 && arg2 != 0 && RimArg1 == 0 && RimArg2 == 0 {
+			if arg1 < 1 && arg1 > 10 && arg2 < 1 && arg2 > 10 {
+				fmt.Println("Вывод ошибки, так как калькулятор принимает на вход числа от 1 до 10 включительно")
+				break
+			}
+
+			switch symbol {
+			case "+":
+				fmt.Println(arg1 + arg2)
+			case "-":
+				fmt.Println(arg1 - arg2)
+			case "/":
+				fmt.Println(arg1 / arg2)
+			case "*":
+				fmt.Println(arg1 * arg2)
+			default:
+				fmt.Println("Вывод ошибки, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *).")
+			}
+		} else {
+			fmt.Println("Вывод ошибки, некорректного ввода")
+			break
+		}
+
+	}
+}
